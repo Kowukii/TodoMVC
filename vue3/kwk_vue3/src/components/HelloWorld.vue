f79e7e64864b6e9ce3967d457bf2b708074cff4e
Kowukii <kowuki7296@gmail.com>
2023-12-12 11:41:34 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   pinia/kwk_pinia/.gitignore
    	new file:   pinia/kwk_pinia/.vscode/extensions.json
    	new file:   pinia/kwk_pinia/README.md
    	new file:   pinia/kwk_pinia/index.html
    	new file:   pinia/kwk_pinia/jsconfig.json
    	new file:   pinia/kwk_pinia/package-lock.json
    	new file:   pinia/kwk_pinia/package.json
    	new file:   pinia/kwk_pinia/public/favicon.ico
    	new file:   pinia/kwk_pinia/src/App.vue
    	new file:   pinia/kwk_pinia/src/assets/base.css
    	new file:   pinia/kwk_pinia/src/assets/logo.svg
    	new file:   pinia/kwk_pinia/src/assets/main.css
    	new file:   pinia/kwk_pinia/src/components/HelloWorld.vue
    	new file:   pinia/kwk_pinia/src/components/TheWelcome.vue
    	new file:   pinia/kwk_pinia/src/components/WelcomeItem.vue
    	new file:   pinia/kwk_pinia/src/components/icons/IconCommunity.vue
    	new file:   pinia/kwk_pinia/src/components/icons/IconDocumentation.vue
    	new file:   pinia/kwk_pinia/src/components/icons/IconEcosystem.vue
    	new file:   pinia/kwk_pinia/src/components/icons/IconSupport.vue
    	new file:   pinia/kwk_pinia/src/components/icons/IconTooling.vue
    	new file:   pinia/kwk_pinia/src/main.js
    	new file:   pinia/kwk_pinia/src/router/index.js
    	new file:   pinia/kwk_pinia/src/stores/counter.js
    	new file:   pinia/kwk_pinia/src/views/AboutView.vue
    	new file:   pinia/kwk_pinia/src/views/HomeView.vue
    	new file:   pinia/kwk_pinia/vite.config.js
    	new file:   vue3/kwk_vue3/.gitignore
    	new file:   vue3/kwk_vue3/README.md
    	new file:   vue3/kwk_vue3/babel.config.js
    	new file:   vue3/kwk_vue3/jsconfig.json
    	new file:   vue3/kwk_vue3/package-lock.json
    	new file:   vue3/kwk_vue3/package.json
    	new file:   vue3/kwk_vue3/public/favicon.ico
    	new file:   vue3/kwk_vue3/public/index.html
    	new file:   vue3/kwk_vue3/src/App.vue
    	new file:   vue3/kwk_vue3/src/assets/logo.png
    	new file:   vue3/kwk_vue3/src/components/HelloWorld.vue
    	new file:   vue3/kwk_vue3/src/main.js
    	new file:   vue3/kwk_vue3/vue.config.js

---
+++ vue3/kwk_vue3/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ myData }}</h1>
+    <button @click="clickHandler">修改</button>
+    <button @click="clickRemove">删除</button>
+  </div>
+</template>
+
+<script setup>
+// 1 reactive / shallowReactive浅响应式
+import { reactive } from 'vue';
+const myData = reactive([1, 2, 3])
+function clickHandler(){
+  myData.push(10)
+  
+}
+// 2 ref / shallowRef
+
+function clickRemove(){
+  myData.pop()
+}
+// 3 reanOnly 只读数据
+// 4 computed
+// 5 watch
+// 6 watchEffect
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
